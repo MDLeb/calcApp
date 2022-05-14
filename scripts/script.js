a696fb64b1e8a015155f0ce57c2ce5fa749f03c4
Mariy <m.lebadina@gmail.com>
2022-05-14 23:55:20 +0300

    fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,68 +1,42 @@
 const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-const actions = {'÷': '/', 'x': '*', '-': '-', '+': '+', '/': '/', '*': '*'};
+const operations = ['/', '*', '-', '+', '/', '*', '.', '%'];
 
-function inputNmb(btn) {
+
+function inputNmb(btn){
     let currentValue = document.querySelector(".display").querySelector("input").value;
-    
-    if (document.querySelector("#operate-handler").value && actions[btn]) {
-        currentValue = document.querySelector(".display").querySelector("input").value;
-        document.querySelector("#operate-handler").value = "false";
-        clearDisplay();
-    }
-    document.querySelector("#operate-handler").value = "false";
-    if (actions[btn]) {
-        verificationMathematicalOperations(currentValue, btn);
-    }
-    else {
-    document.querySelector(".display").querySelector("input").value = `${currentValue}${numbers[btn]}`;
-    }
+    document.querySelector(".display").querySelector("input").value = `${currentValue}${btn}`;
     document.querySelector("input").focus();    
 }
 
-function operate() {
-   let result = eval(document.querySelector(".display").querySelector("input").value);
-   document.querySelector(".display").querySelector("input").value = result;
-   document.querySelector("#operate-handler").value = "true";
+function operate(event){
+    if (event.code != 'Enter' && event.code != 'NumpadEnter' && event.type != 'click') return;
+    let str = document.querySelector(".display").querySelector("input").value;
+    if (str.includes('%')) str = str.replace('%', '*0.01');
+    let result = eval(str);
+    document.querySelector(".display").querySelector("input").value = result;
 }
 
-function percentage() {
-    let currentValue = document.querySelector(".display").querySelector("input").value;
-    document.querySelector(".display").querySelector("input").value = currentValue + "*0.01";
-    operate();
-}
 function changeValue(){
     let currentValue = document.querySelector(".display").querySelector("input").value;
     document.querySelector(".display").querySelector("input").value = -currentValue;
 }
-function clearDisplay() {
+
+function clearDisplay(){
     document.querySelector(".display").querySelector("input").value = null;
 }
-function handleEnteredData(event) {
-    let val = event.target.value;
-    let operations = [];
-    for (let operate in actions) {
-        operations.push(actions[operate]);
-    }
-    if(operations.includes(val[val.length - 1]) && operations.includes(event.key)) {
-        console.log('++');
-        event.target.value = `${event.target.value.slice(0, (val.length - 1))}`;
-    }
-    // if (exp.test(val[val.length - 1]) && exp.test(event.key)) {
-    // //     verificationMathematicalOperations(event.value, event.value.slice(-1), true);
-    //     console.log('+++');
-    // }
-    // console.log(exp.test(val[val.length - 1]));
-    // console.log(exp.test(event.key))
-    // console.log('------');
 
-}
-function verificationMathematicalOperations(currentValue, btn, isKeyboard) {
-    let lastSymbol = isKeyboard ? currentValue.slice(-2, -1) : currentValue.slice(-1);
-    if (lastSymbol==="/"||lastSymbol==="*"||lastSymbol==="-"||lastSymbol==="+") {
-        document.querySelector(".display").querySelector("input").value = isKeyboard ?
-        currentValue.slice(0, currentValue.length-2) + actions[btn] : currentValue.slice(0, currentValue.length-1) + actions[btn];
-    } else {
-        document.querySelector(".display").querySelector("input").value = isKeyboard ?
-        currentValue : `${currentValue}${actions[btn]}`;
+function handleEnteredData(event){
+    let val = event.target.value;
+    if (val == '' && operations.includes(event.key)) event.target.value = '0';
+
+    if (operations.includes(val[val.length - 1]) && operations.includes(event.key)) event.target.value = `${event.target.value.slice(0, (val.length - 1))}`;
+    if (event.keyCode >= 65 && event.keyCode <= 90 || 
+       [221, 219, 186, 222, 188, 190].includes(event.keyCode) || 
+       (event.shiftKey && event.keyCode !== 53)) {
+       event.preventDefault();
+    }
+    if (event.keyCode == 110) {
+        let arr = val.split(/[-/*/+/]/);
+        if (arr[arr.length-1].includes('.')) event.preventDefault();
     }
 }
